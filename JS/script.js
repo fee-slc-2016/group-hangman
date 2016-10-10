1bb4b95c8ebbdb67e591347c620f7361cea4f7aa
trrapp@gmail.com <trrapp@gmail.com>
2016-10-10 10:37:49 -0600

    feat(core) Take user guess via prompt
    Fixed #7

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,9 @@
 "use strict";
 
 console.log('cheese and puppies');
+
+let char = prompt ('What character would you like to try next?');
+
+ // Note that the console.log below uses two parameters.  By default it will autopopulate the space between the two.  This is only if they are both strings.
+
+console.log('the player is trying', char);
