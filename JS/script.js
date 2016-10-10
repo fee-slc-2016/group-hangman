6de22e3116432c80cd4c19218b43b36542d6aec4
DrewWarrenTIY <drewwarrentiy@gmail.com>
2016-10-10 11:36:44 -0600

    feat(init) Add function to create status;
    Populates status with an underscore for each letter in answer
    Closes #16

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,18 @@ let char = prompt ('What character would you like to try next?');
  // Note that the console.log below uses two parameters.  By default it will autopopulate the space between the two.  This is only if they are both strings.
 
 console.log('the player is trying', char);
+
+// We are using 'cheese' as the answer and 'e' as the guess for now.
+
+let answer = "cheese";
+let status = createStatus(answer);
+let guess = "e";
+
+function createStatus (answer_string) {
+  let status_array = [];
+  for (let i = 0; i < answer_string.length; i++) {
+    status_array.push("_");
+  }
+  return status_array;
+}
+console.log(status);
